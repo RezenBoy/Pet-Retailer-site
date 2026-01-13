5485d69d1b47bb383979d378911f4320144f7aae
RezenBoy <officalproton@gmail.com>
2026-01-13 18:35:28 +0530

    version 1.0.2 padding issue

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,8 +7,7 @@ function Home() {
       <Hero />
 
       <section className="bg-slate-50 py-10">
-        <div className="max-w-7xl mx-auto px-20">
-
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-20">
           <h2 className="text-2xl font-semibold text-slate-800 mb-2">
             Featured Products
           </h2>
